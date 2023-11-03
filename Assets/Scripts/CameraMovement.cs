b682a02ecdba163be1f32919c73d1a9df23fc655
Miko Grönroos <miko.gronroos@gmail.com>
2023-11-03 02:18:19 +0200

    Added constraints to camera movement

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -8,12 +8,18 @@ public class CameraMovement : MonoBehaviour
 
     [SerializeField] private float movementSpeed;
 
+    [SerializeField] private float maxY;
+    [SerializeField] private float minY;
+
+    [SerializeField] private float maxX;
+    [SerializeField] private float minX;
+
     [SerializeField] private Vector3 movementVector;
     private Vector3 velocity = Vector3.zero;
     void Update()
     {
-        movementVector.x = transform.position.x + Input.GetAxisRaw("Horizontal") * movementSpeed * Time.deltaTime;
-        movementVector.y = transform.position.y + Input.GetAxisRaw("Vertical") * movementSpeed * Time.deltaTime;
+        movementVector.x = Mathf.Clamp(transform.position.x + Input.GetAxisRaw("Horizontal") * movementSpeed * Time.deltaTime, minX, maxX);
+        movementVector.y = Mathf.Clamp(transform.position.y + Input.GetAxisRaw("Vertical") * movementSpeed * Time.deltaTime, minY, maxY); 
         movementVector.z = transform.position.z;
 
         transform.position = Vector3.SmoothDamp(transform.position, movementVector, ref velocity, Time.deltaTime);
